8076f006de50af4a33dc2b501676ce72ae1a068b
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2025-06-23 21:25:08 -0400

    fix dropdowns

--- src/components/ui/searchable-dropdown.tsx
+++ src/components/ui/searchable-dropdown.tsx
@@ -163,8 +163,11 @@ export function SearchableDropdown({
       </PopoverTrigger>
       <PopoverContent 
         className="w-[--radix-popover-trigger-width] p-0 z-50 overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2" 
+        side="bottom"
         align="start"
         sideOffset={4}
+        avoidCollisions={false}
+        sticky="always"
       >
         <div className="flex flex-col">
           <div className="p-2 border-b">
